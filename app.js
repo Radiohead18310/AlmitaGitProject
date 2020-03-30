25d24ceeb2d9ec9a923ba60bc6a328d4454cb500
jvmojuan228 <jvmo.morales5a@hotmail.com>
2020-03-30 13:02:57 -0600

    Se realizo la conexción a la base de datos por medio de MySQL y se mostró el primer 'Hola Mundo'.

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express= require('express');
 const morgan = require('morgan');
 const path= require('path');
+const mysql= require('mysql');
 const myConnection= require('express-myconnection');
 const handlebars= require ('express-handlebars');
 const session = require('cookie-session');
 const app= express();
 //-------------------------------------Configuracion del puerto----------------------------------------------------------------
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 8000);
 //-------------------------------------Configuracion del engine de plantillas------------------------------------------------
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', handlebars({
@@ -19,6 +20,14 @@ app.set('view engine', '.hbs');
 //------------------------------------Configuracion de Midlewares------------------------------------------------
 app.use(express.json());
 app.use(morgan('dev'));
+//////////////////////Conneccion con mysql///////////////////
+app.use(myConnection(mysql, {
+    host: 'localhost',
+    user: 'root',
+    password: 'n0m3l0',
+    port: 3306,
+    database: 'BaseGit'
+}, 'single'));
 app.use('/', require(path.join(__dirname, 'routes/routes')));
 app.use(express.static(path.join(__dirname, 'public')));
 //------------------------------------ABRIENDO EL SERVER----------------------------------------------------------------------
